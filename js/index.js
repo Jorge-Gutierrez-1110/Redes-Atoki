0c45fc052baf60738efa0d18be9329a7f1d139e0
Jorge-Gutierrez-1110 <114625146+Jorge-Gutierrez-1110@users.noreply.github.com>
2025-12-10 05:16:24 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const apiUrl = 'https://decapi.me/youtube/latest_video?id=UCOax8ucPa34i6-Vs87VKykw';
+
+fetch(apiUrl)
+    .then(response => response.text())
+    .then(data => {
+        console.log("Datos recibidos de StreamElements:", data);
+
+        const match = data.match(/youtu\.be\/([a-zA-Z0-9_-]+)/);
+
+        if (match && match[1]) {
+            const videoId = match[1];
+
+            const iframe = document.getElementById('youtube-frame');
+            iframe.src = `https://www.youtube.com/embed/${videoId}`;
+
+            document.getElementById('loading-text').style.display = 'none';
+        } else {
+            console.error("No se pudo encontrar el ID del video en la respuesta.");
+        }
+    })
+    .catch(error => {
+        console.error('Error al obtener el video:', error);
+        document.getElementById('loading-text').innerText = "No se pudo cargar el video.";
+    });
