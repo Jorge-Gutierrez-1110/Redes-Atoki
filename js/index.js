f65f7dc2f4ddf42bdc7eea00f4feab541b8b4e2c
Jorge-Gutierrez-1110 <114625146+Jorge-Gutierrez-1110@users.noreply.github.com>
2025-12-12 19:36:02 -0600

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -9,10 +9,8 @@ fetch(apiUrl)
 
         if (match && match[1]) {
             const videoId = match[1];
-
             const iframe = document.getElementById('youtube-frame');
             iframe.src = `https://www.youtube.com/embed/${videoId}`;
-
             document.getElementById('loading-text').style.display = 'none';
         } else {
             console.error("No se pudo encontrar el ID del video en la respuesta.");
@@ -26,22 +24,21 @@ fetch(apiUrl)
 const twitchUser = 'atoki_1110';
 const twitchApiUrl = `https://decapi.me/twitch/uptime/${twitchUser}`;
 
-        fetch(twitchApiUrl)
-            .then(response => response.text())
-            .then(data => {
-                console.log("Estado Twitch:", data);
+fetch(twitchApiUrl)
+    .then(response => response.text())
+    .then(data => {
+        console.log("Estado Twitch:", data);
 
-                if (!data.includes("offline")) {
-                    const twitchBtn = document.querySelector('.twitch');
-                    
-                    if (twitchBtn) {
-                        twitchBtn.innerHTML = '<i class="fab fa-twitch"></i> 🔴 ¡ESTOY EN VIVO!';
-                        
-                        twitchBtn.classList.add('live-mode');
-                        
-                        const linksContainer = document.querySelector('.links');
-                        linksContainer.prepend(twitchBtn);
-                    }
-                }
-            })
-            .catch(err => console.error('Error Twitch:', err));
+        if (!data.includes("offline")) {
+            const twitchBtn = document.querySelector('.twitch');
+
+            if (twitchBtn) {
+                twitchBtn.innerHTML = '<i class="fab fa-twitch"></i> 🔴 ¡ESTOY EN VIVO!';
+                twitchBtn.classList.add('live-mode');
+
+                const linksContainer = document.querySelector('.links');
+                linksContainer.prepend(twitchBtn);
+            }
+        }
+    })
+    .catch(err => console.error('Error Twitch:', err));
